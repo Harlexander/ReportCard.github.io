68d65acfd841d1b8925882c10a0eabf686c64ac9
Harlexander <dunkwualex6@gmail.com>
2020-09-10 00:10:53 +0100

    Added some javascript features including classes

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const columns = [1,2,3,4,5];
+let ca, md, exam, agg;
+let [totalca, totalmd, totalexam, total] = [0, 0, 0, 0];
 
-let _student,_grade, _dep, _sex, _teacher, _session;
 window.onload = () => {
+  //events that ll occcurs on screen load
     showHide("form","block")
     showHide("report", "none")
     document.getElementById("student").value = get("name");
@@ -9,40 +12,90 @@ window.onload = () => {
     document.getElementById("dep").value = get("dep");
     document.getElementById("sex").value = get("sex");
     document.getElementById("session").value = get("session");
+
+     
 }
+
+  class StudentInfo {
+      constructor (student, grade, dep, sex, teacher, session){
+        this.student = student;
+        this.grade = grade;
+        this.dep = dep;
+        this.sex = sex;
+        this.teacher = teacher;
+        this.session = session;
+      }
+    }
 function myFunction() {
-    _student = document.getElementById("student").value;
-    _grade = document.getElementById("grade").value;
-    _dep =  document.getElementById("dep").value;
-    _sex = document.getElementById("sex").value;
-    _teacher = document.getElementById("teacher").value;
-    _session = document.getElementById("session").value;
-
-    storage("name", _student);
-    storage("teacher", _teacher);
-    storage("grade", _grade);
-    storage("dep", _dep);
-    storage("sex", _sex);
-    storage("session", _session);
+    studentBio = new StudentInfo(document.getElementById("student").value, 
+    document.getElementById("grade").value, 
+    document.getElementById("dep").value, 
+    document.getElementById("sex").value,
+    document.getElementById("teacher").value, 
+    document.getElementById("session").value)
 
 
-    if( _student!="" && _teacher != ""){
+    storage("name", studentBio.student);
+    storage("teacher", studentBio.teacher);
+    storage("grade", studentBio.grade);
+    storage("dep", studentBio.dep);
+    storage("sex", studentBio.sex);
+    storage("session", studentBio.session);
+
+
+    if( studentBio.student!="" && studentBio.teacher!= ""){
         showHide("form", "none")
         showHide("report", "block");
 
         personalInfo("fullname", get("name"));
-        personalInfo("class", _grade);
-        personalInfo("dept", _dep);
-        personalInfo("gender", _sex);
-        personalInfo("teachername", _teacher);
-        personalInfo("term", _session);
-
-        document.getElementById("date").innerHTML = new Date().getDate;
-
+        personalInfo("class", get("grade"));
+        personalInfo("dept", get("dep"));
+        personalInfo("gender", get("sex"));
+        personalInfo("teachername", get("teacher"));
+        personalInfo("term", get("session"));
     }else{
         alert("please fill all")
     }
   }
+  for(const x of columns){
+      document.getElementById("button").addEventListener('click', function(){
+        
+        ca = parseInt(document.getElementById("ca" + x).value);
+        md = parseInt(document.getElementById("mt" + x).value);
+        exam = parseInt(document.getElementById("exam" + x).value);
+        agg = document.getElementById("aggregate" + x).innerHTML = (ca + md + exam)/2; 
+
+        if (ca == "" || md == "" || exam == "") {
+          return alert("input all scores")
+         
+        }
+        document.getElementById("totalscoreca").innerHTML =  Math.floor(totalca += ca);
+        document.getElementById("totalscoremt").innerHTML =  Math.floor(totalmd += md);
+        document.getElementById("totalscoreexam").innerHTML =  Math.floor(totalexam += exam);
+        document.getElementById("totalscore").innerHTML =  Math.floor((total += agg)/500 * 100) + "%";
+        
+        if(agg > 70){
+          document.getElementById("performance" + x).style.backgroundColor = "green";
+          document.getElementById("performance" + x).innerHTML = "A+";
+        }else if(agg > 59 && agg< 70){
+          document.getElementById("performance" + x).style.backgroundColor = "blue";
+          document.getElementById("performance" + x).innerHTML = "B+";
+        }else if(agg > 49 && agg< 60){
+          document.getElementById("performance" + x).style.backgroundColor = "yellow";
+          document.getElementById("performance" + x).innerHTML = "C+";         
+        }else if(agg < 49){
+          document.getElementById("performance" + x).style.backgroundColor = "red";
+          document.getElementById("performance" + x).innerHTML = "F";  
+        }else{
+          document.getElementById("performance" + x).style.backgroundColor = "white";
+          document.getElementById("performance" + x).innerHTML = ""; 
+        }
+        showHide("button", "none")
+      })
+  }
+    
+        
+        
 
 const storage = (key, value) => {
   return sessionStorage.setItem(key, value)
